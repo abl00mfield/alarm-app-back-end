cc853f7d91229e953b8f87625ad55c4eafd63a57
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-04 11:56:15 -0600

    added alarm model

---
+++ models/alarm.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const alarmSchema = new moongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name: {
+      //the application will contain a drop down list of preloaded tones
+      type: String,
+      required: true,
+    },
+    time: {
+      //the time the alarm will go off
+      type: String,
+      requried: true,
+    },
+    tone: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tone",
+    },
+    snooze: {
+      type: Boolean,
+      default: false,
+    },
+    active: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Alarm = mongoose.model("Alarm", alarmSchema);
+module.exports = Alarm;
