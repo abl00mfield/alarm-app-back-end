03ff0623305ad34bd5a21d94bde4ef88597fa0b3
joally <joally14@gmail.com>
2025-04-07 00:17:25 -0600

    wip: created route to read all alarms

--- controllers/alarms.js
+++ controllers/alarms.js
@@ -8,14 +8,24 @@ const router = express.Router();
 // add routes here
  router.post("/", verifyToken, async (req, res) => {
     try {
-        req.body.author = req.user._id;
+        req.body.owner = req.user._id;
       const alarm = await Alarm.create(req.body);
-      alarm._doc.author = req.user;
+      alarm._doc.owner = req.user;
       res.status(201).json(alarm);
     } catch (error) {
        res.status(500).json({ error: error.message });
     }
   });
-  
+  router.get("/", verifyToken, async (req, res) => {
+    try {
+      const alarms = await Alarm.find({})
+        .populate("owner")
+        .sort({ createdAt: "desc" });
+      res.status(200).json(alarms);
+    } catch (error) {
+        console.log(error);
+      res.status(500).json({ err: err.message });
+    }
+  });
 
 module.exports = router;
