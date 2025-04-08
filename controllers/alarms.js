6703854179b804dea37b9ebb6c488f9236000900
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-07 20:33:32 -0600

    populated the tone information into the show route for a single alarm

--- controllers/alarms.js
+++ controllers/alarms.js
@@ -3,94 +3,78 @@ const verifyToken = require("../middleware/verify-token.js");
 const Alarm = require("../models/alarm");
 const router = express.Router();
 
-
-
-
- router.post("/", verifyToken, async (req, res) => {
-    try {
-        req.body.owner = req.user._id;
-      const alarm = await Alarm.create(req.body);
-      alarm._doc.owner = req.user;
-      res.status(201).json(alarm);
-    } catch (error) {
-       res.status(500).json({ error: error.message });
-    }
-  });
-  router.get("/", verifyToken, async (req, res) => {
-    try {
-      const alarms = await Alarm.find({})
-        .populate("owner" )
-        .populate("tone")
-        .sort({ time: "asc" });
-      res.status(200).json(alarms);
-    } catch (error) {
-        console.log(error);
-      res.status(500).json({ err: err.message });
-    }
-  });
-
-  router.get("/:alarmId", verifyToken, async (req, res) => {
-    try {
-     const alarm = await Alarm.findById(req.params.alarmId).populate([
-        "owner",
-       
-      ]);
-      res.status(200).json(alarm);
-    } catch (err) {
-      res.status(500).json({ err: err.message });
-    }
-  });
-
-  router.put("/:alarmId", verifyToken, async (req, res) => {
-    try {
-      const alarm = await Alarm.findById(req.params.alarmId);
-      if (!alarm) {
-        return res.status(404).send("Alarm not found");
-      }
-    
-
-      if (!alarm.owner|| !alarm.owner.equals(req.user._id)) {
-          return res.status(403).send("You're not allowed to do that!");
-        }
-        const updatedAlarm = await Alarm.findByIdAndUpdate(
-            req.params.alarmId,
-            req.body,
-            { new: true }
-        );
-        updatedAlarm._doc.owner = req.user;
-        res.status(200).json(updatedAlarm);
-        } catch (err) {
-            console.log(err);
-        res.status(500).json({ err: err.message });
+router.post("/", verifyToken, async (req, res) => {
+  try {
+    req.body.owner = req.user._id;
+    const alarm = await Alarm.create(req.body);
+    alarm._doc.owner = req.user;
+    res.status(201).json(alarm);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  });
+});
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const alarms = await Alarm.find({})
+      .populate("owner")
+      .populate("tone")
+      .sort({ time: "asc" });
+    res.status(200).json(alarms);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ err: err.message });
+  }
+});
 
-  router.delete("/:alarmId", verifyToken, async (req, res) => {
-    try {
-      const alarm = await Alarm.findById(req.params.alarmId);
-  
-      if (!alarm.owner.equals(req.user._id)) {
-        return res.status(403).send("You're not allowed to do that!");
-      }
-  
-      const deletedAlarm = await Alarm.findByIdAndDelete(req.params.alarmId);
-      res.status(200).json(deletedAlarm);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: error.message });
+router.get("/:alarmId", verifyToken, async (req, res) => {
+  try {
+    const alarm = await Alarm.findById(req.params.alarmId).populate([
+      "owner",
+      "tone",
+    ]);
+    res.status(200).json(alarm);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+router.put("/:alarmId", verifyToken, async (req, res) => {
+  try {
+    const alarm = await Alarm.findById(req.params.alarmId);
+    if (!alarm) {
+      return res.status(404).send("Alarm not found");
     }
-  });
- 
-     
-     
-     
-       
-  
-  
 
-  
+    if (!alarm.owner || !alarm.owner.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+    const updatedAlarm = await Alarm.findByIdAndUpdate(
+      req.params.alarmId,
+      req.body,
+      { new: true }
+    );
+    updatedAlarm._doc.owner = req.user;
+    res.status(200).json(updatedAlarm);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: err.message });
+  }
+});
 
+router.delete("/:alarmId", verifyToken, async (req, res) => {
+  try {
+    const alarm = await Alarm.findById(req.params.alarmId);
 
+    if (!alarm.owner.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
 
+    const deletedAlarm = await Alarm.findByIdAndDelete(req.params.alarmId);
+    res.status(200).json(deletedAlarm);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
